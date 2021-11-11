ff325cb30a9951dbedaea9528c173d0052a78236
segevmalool <segbaus@gmail.com>
2021-11-11 16:35:52 -0500

    feature: stream rows from db

--- spring-security-webflux-postgres/src/main/java/com/segbaus/customer/CustomerController.java
+++ spring-security-webflux-postgres/src/main/java/com/segbaus/customer/CustomerController.java
@@ -2,6 +2,7 @@ package com.segbaus.customer;
 
 import io.r2dbc.spi.ConnectionFactory;
 import java.util.UUID;
+import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-@Component
 @RestController
 public class CustomerController {
 
@@ -19,9 +19,8 @@ public class CustomerController {
     this.client = DatabaseClient.create(connectionFactory);
   }
 
-  @GetMapping("/customers")
+  @GetMapping(value ="/customers", produces=MediaType.TEXT_EVENT_STREAM_VALUE)
   public Flux<UUID> save() {
     return client.sql("select * from customer;").map(row -> (UUID) row.get("id")).all();
   }
-  ;
 }
