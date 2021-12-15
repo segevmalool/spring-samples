065ae795b04e574469d188a97cbaeddaadfed5e4
segevmalool <segbaus@gmail.com>
2021-12-15 09:48:28 -0500

    apply spotless

--- spring-reactive-resource-server/src/main/java/com/segbaus/config/SecurityConfiguration.java
+++ spring-reactive-resource-server/src/main/java/com/segbaus/config/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package com.segbaus.config;
 
+import java.beans.IntrospectionException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.annotation.Bean;
@@ -16,11 +20,6 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.beans.IntrospectionException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 
@@ -29,25 +28,25 @@ public class SecurityConfiguration {
   @Bean
   public SecurityWebFilterChain webFilterChain(ServerHttpSecurity http) {
     http.authorizeExchange()
-      .pathMatchers("/actuator/**")
-      .access(
-        (authenticationMono, context) ->
-          Mono.just(new AuthorizationDecision(true))
-            .doOnNext((authn) -> this.log.info("hello")))
-      .anyExchange()
-      .authenticated()
-      .and()
-      .oauth2ResourceServer()
-      .opaqueToken()
-      .introspector(new GoogleIntrospector())
-      .and()
-      .and()
-      .httpBasic()
-      .disable()
-      .formLogin()
-      .disable()
-      .csrf()
-      .disable();
+        .pathMatchers("/actuator/**")
+        .access(
+            (authenticationMono, context) ->
+                Mono.just(new AuthorizationDecision(true))
+                    .doOnNext((authn) -> this.log.info("hello")))
+        .anyExchange()
+        .authenticated()
+        .and()
+        .oauth2ResourceServer()
+        .opaqueToken()
+        .introspector(new GoogleIntrospector())
+        .and()
+        .and()
+        .httpBasic()
+        .disable()
+        .formLogin()
+        .disable()
+        .csrf()
+        .disable();
     return http.build();
   }
 
@@ -56,61 +55,55 @@ public class SecurityConfiguration {
     private final String googleTokenInfoEndpoint = "https://oauth2.googleapis.com/tokeninfo";
 
     private record GoogleIntrospectionResponse(
-      String azp,
-      String aud,
-      String sub,
-      String scope,
-      String exp,
-      String expires_in,
-      String email,
-      String email_verified,
-      String access_type
-    ) {
-    }
+        String azp,
+        String aud,
+        String sub,
+        String scope,
+        String exp,
+        String expires_in,
+        String email,
+        String email_verified,
+        String access_type) {}
 
-    private record GoogleIntrospectionError(
-      String error,
-      String error_description
-    ) {
-    }
+    private record GoogleIntrospectionError(String error, String error_description) {}
 
     public Mono<OAuth2AuthenticatedPrincipal> introspect(String access_token) {
-      return WebClient.create(googleTokenInfoEndpoint).get().uri(
-          uriBuilder -> uriBuilder.queryParam("access_token", access_token).build()
-        )
-        .retrieve()
-        .onStatus(status -> status.equals(HttpStatus.BAD_REQUEST), clientResponse ->
-          Mono.error(new IntrospectionException("Invalid token"))
-        )
-        .bodyToMono(GoogleIntrospectionResponse.class)
-        .doOnNext(googleIntrospectionResponse -> {
-          log.info(googleIntrospectionResponse);
-        })
-        .flatMap(googleIntrospectionResponse -> {
-          Map<String, Object> attributes = getAttributes(googleIntrospectionResponse);
+      return WebClient.create(googleTokenInfoEndpoint)
+          .get()
+          .uri(uriBuilder -> uriBuilder.queryParam("access_token", access_token).build())
+          .retrieve()
+          .onStatus(
+              status -> status.equals(HttpStatus.BAD_REQUEST),
+              clientResponse -> Mono.error(new IntrospectionException("Invalid token")))
+          .bodyToMono(GoogleIntrospectionResponse.class)
+          .doOnNext(
+              googleIntrospectionResponse -> {
+                log.info(googleIntrospectionResponse);
+              })
+          .flatMap(
+              googleIntrospectionResponse -> {
+                Map<String, Object> attributes = getAttributes(googleIntrospectionResponse);
 
-          List<GrantedAuthority> authorities = parseScopeAuthorities(googleIntrospectionResponse.scope, attributes);
-          authorities.add(new OAuth2UserAuthority("ROLE_USER", attributes));
+                List<GrantedAuthority> authorities =
+                    parseScopeAuthorities(googleIntrospectionResponse.scope, attributes);
+                authorities.add(new OAuth2UserAuthority("ROLE_USER", attributes));
 
-          return Mono.just(
-            new OAuth2IntrospectionAuthenticatedPrincipal(
-              attributes,
-              authorities
-            )
-          );
-        });
+                return Mono.just(
+                    new OAuth2IntrospectionAuthenticatedPrincipal(attributes, authorities));
+              });
     }
 
-    private Map<String, Object> getAttributes(GoogleIntrospectionResponse googleIntrospectionResponse) {
+    private Map<String, Object> getAttributes(
+        GoogleIntrospectionResponse googleIntrospectionResponse) {
       return Map.of(
-        "aud", googleIntrospectionResponse.aud,
-        "sub", googleIntrospectionResponse.sub,
-        "email", googleIntrospectionResponse.email,
-        "email_verified", googleIntrospectionResponse.email_verified
-      );
+          "aud", googleIntrospectionResponse.aud,
+          "sub", googleIntrospectionResponse.sub,
+          "email", googleIntrospectionResponse.email,
+          "email_verified", googleIntrospectionResponse.email_verified);
     }
 
-    private List<GrantedAuthority> parseScopeAuthorities(String oauth2Scopes, Map<String, Object> attributes) {
+    private List<GrantedAuthority> parseScopeAuthorities(
+        String oauth2Scopes, Map<String, Object> attributes) {
       String[] scopes = oauth2Scopes.split(" ");
       List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 
